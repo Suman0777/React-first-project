e3700f9e2f6887f3cd94881fd9c15e47bb90242b
suman basak <gorangobasakpkr@gmail.com>
2025-09-17 03:15:36 +0530

    The new changes

--- react-project/src/body/b.jsx
+++ react-project/src/body/b.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import './styll.css'
 export default function b() {
   return (
-    <div>b</div>
+    <div>
+      <div className='Container'>
+          <div className='common'>health</div>
+          <div className='common'>Music</div>
+          <div className='common'>Hpoe</div>
+          <div className='common'>Wish</div>
+      </div>
+    </div>
   )
 }
